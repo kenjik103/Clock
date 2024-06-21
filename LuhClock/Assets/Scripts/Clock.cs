07a3ce11384e70bf279e17de3a7eb646ec5ace7f
Kenjiro Kanagy <kenjik103@gmail.com>
2024-06-21 12:53:00 -0700

    Uploading project

---
+++ LuhClock/Assets/Scripts/Clock.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+
+    void Awake() {
+        hoursPivot.localRotation = Quaternion.Euler(0,0,-30);
+    }
+}
